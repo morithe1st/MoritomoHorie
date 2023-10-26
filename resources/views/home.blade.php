52007a804f5318f0c1d1d2945e36bb7760fa8d55
morithe1st <133379162+morithe1st@users.noreply.github.com>
2023-10-26 17:31:44 +0900

    231026

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,12 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>ようこそ　商品管理システムへ</h1>
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <p>こちらはインテリア商品管理システムです。</p>
+    <p>サイドバーから行いたい作業を実施してください。</p>
 @stop
 
 @section('css')
